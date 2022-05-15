c7f55f10d6717cdae220a2006bf692ffb9c59bed
Haris Hambasic <HarisHambasic@Adiss-MacBook.local>
2022-05-14 19:13:00 -0500

    Prepared for deployment

--- backend/server.js
+++ backend/server.js
@@ -50,9 +50,9 @@ cs340_project_server.use((req, res, next) => {
 //     return;
 // });
 
-cs340_project_server.use(cors());
 
 // MIDDLEWARE
+cs340_project_server.use(cors());
 cs340_project_server.use(express.json());
 cs340_project_server.use(express.urlencoded({ extended: true }));
 cs340_project_server.use(bodyParser.json());
@@ -250,6 +250,20 @@ cs340_project_server.get("/api/students", (req, res) => {
         };
     });
 });
+
+if (process.env.NODE_ENV === "production") {
+
+    // Set static folder
+    // All the javascript and css files will be read and served from this folder
+    cs340_project_server.use(express.static("client/build"));
+  
+    // index.html for all page routes  html or routing and naviagtion
+    cs340_project_server.get("*", (req, res) => {
+      res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
+    });
+
+    console.log("Succesfull");
+}
 
 cs340_project_server.listen(PORT, () => {
     console.log(`Connected to server at port ${PORT}`);
